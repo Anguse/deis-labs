b3989df2c251615023bf40b22a73d0ebe276ae27
LazioSweden <kgantonolsson@gmail.com>
2019-10-23 11:57:41 +0200

    added lane-following

--- project/actuator/actuator/actuator.ino
+++ project/actuator/actuator/actuator.ino
@@ -64,7 +64,45 @@ void loop(){
       }
       newData = false;
     }
-   if(mode == LINE_FOLLOLWING){
+  if(mode == LINE_FOLLOWING){
        //LINE FOLLOWING CODE GOES HERE
+     //custom controls
+    // if outside sensors are equal (both white or both black) and not all middle sensors are black -> move straight
+    if((((left_outer.read() > LINETHRESHOLD) && (right_outer.read() > LINETHRESHOLD)) || ((left_outer.read() < LINETHRESHOLD) && (right_outer.read() < LINETHRESHOLD))) 
+          && !((left.read() > LINETHRESHOLD) && (center.read() > LINETHRESHOLD) && (right.read() > LINETHRESHOLD)))
+    {
+      leftSpeed = -SPEED; 
+      rightSpeed = SPEED;
+    }
+    // if only left is black -> move to right
+    else if((left_outer.read() > LINETHRESHOLD) && (right_outer.read() < LINETHRESHOLD))
+    {
+      leftSpeed = -(SPEED);
+      rightSpeed = 0;
+    }
+    // if only right is black -> move to left
+    else if((right_outer.read() > LINETHRESHOLD) && (left_outer.read() < LINETHRESHOLD))
+    {
+      leftSpeed = 0;
+      rightSpeed = SPEED;
+    }
+    else{
+      leftSpeed = 0;
+      rightSpeed = 0;
+    }
+    
+    // if all sensors are on black or up in the air, stop the motors.
+    // otherwise, run motors given the control speeds above.
+    if((left.read() > LINETHRESHOLD) && (center.read() > LINETHRESHOLD) && (right.read() > LINETHRESHOLD) && (right_outer.read() > LINETHRESHOLD) && (left_outer.read() > LINETHRESHOLD) )
+    {
+      motors.stop();
+    }
+    else
+    {
+      motors.leftMotor(leftSpeed);
+      motors.rightMotor(rightSpeed);
+    } 
+
+       
    }
 }  
